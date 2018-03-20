794c8648a8c1b209fc8b5636969e716d0c176fe6
Thiago Monteiro da Silva <thiagom.silva195@gmail.com>
2018-03-20 13:22:48 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ projetoteste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoteste {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Ola mundo");
+            Console.ReadLine();
+        }
+    }
+}
