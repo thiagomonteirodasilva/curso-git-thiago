614d5d1c6b6221172d7747051ebd0504e694b0b3
Thiago Monteiro da Silva <thiagom.silva195@gmail.com>
2018-03-20 14:34:50 -0300

    Acrescentada a mensagem desejando um bom dia

--- projetoteste/Program.cs
+++ projetoteste/Program.cs
@@ -8,8 +8,8 @@ namespace projetoteste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Ola mundo");
-            Console.WriteLine("Bom dia");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia");
             Console.ReadLine();
         }
     }
